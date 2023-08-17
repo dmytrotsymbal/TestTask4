500c83b4e6b834e6ee09ecc152fb1012886f358a
Dima <dima.tsymbal@outlook.com>
2023-08-17 13:55:12 +0300

    add burgerMenu

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
 //     document.getElementById("hiddenInput").style.display = "block";
 //     headerLinks.style.minWidth = "600px";
 //   });
+
+//-----------------------------------------------------------------
+
+const adaptiveBtn = document.getElementById("adaptiveBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const closeAdaptiveMenu = document.getElementById("closeAdaptiveMenu");
+
+adaptiveBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
+
+closeAdaptiveMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
