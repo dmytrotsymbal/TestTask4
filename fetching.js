23c136b9300bf38d7361812f461a757cd43b831a
Dima <dima.tsymbal@outlook.com>
2023-08-18 15:12:40 +0300

    fetching processing

---
+++ fetching.js
@@ -0,0 +1,40 @@
+const fetchImagesContainer = document.getElementById("fetchImages");
+
+async function fetchImages() {
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random?client_id=s2iDMNtNY-yGTP-Q8T1X3dNDY8Dw3vzuBE6T1ia07hg&count=9"
+  );
+  const data = await response.json();
+  return data;
+}
+
+function createPhotoElement(photo) {
+  const img = document.createElement("img");
+  img.src = photo.urls.small;
+  img.alt = photo.alt_description;
+  img.classList.add("photo");
+
+  img.addEventListener("load", () => {
+    new Masonry(fetchImagesContainer, {
+      itemSelector: ".photo",
+      columnWidth: fetchImagesContainer.offsetWidth * 0.32,
+      gutter: 25,
+    });
+  });
+
+  return img;
+}
+
+async function displayImages() {
+  try {
+    const images = await fetchImages();
+    images.forEach((photo) => {
+      const photoElement = createPhotoElement(photo);
+      fetchImagesContainer.appendChild(photoElement);
+    });
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+displayImages();
